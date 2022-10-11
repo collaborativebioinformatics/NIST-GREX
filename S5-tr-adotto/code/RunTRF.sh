97b0395829886417a9de788ebe2d57cc0ab59dd3
bharatij <bharatijadhav@gmail.com>
2022-10-11 19:02:07 -0400

    Update RunTRF.sh

--- S5-tr-adotto/code/RunTRF.sh
+++ S5-tr-adotto/code/RunTRF.sh
@@ -3,8 +3,8 @@
 
 #Goal : Run Tandem Repeat Finder on TR annotation region from adotto_TRannotations_v0.2.bed.gz to genearte % of A,C,G,T, matches and indels which will be used as a features in EBM pipeline
 
-###create fasta for trf
-cat data/adotto_TRannotations_v0.2_json_flat.bed |grep -v ^#chrom | awk '{print $1 ":" $2 "-" $3}' |samtools faidx -r - Homo_sapiens_assembly38.fasta > data/adotto_TRannotations_Regions.fasta
+###create fasta for trf (TR annotations have 0-based start and end coordinates) 
+zgrep -v ^ #chrom data/adotto_TRannotations_v0.2_json_flat.bed.gz |cut -f1-3 |sort | uniq |bedtools sort -i - | awk '{print $1 ":" $2 "-" $3+1}'| samtools faidx -r - Homo_sapiens_assembly38.fasta > data/adotto_TRannotations_Regions.fasta
 
 trf data/adotto_TRannotations_Regions.fasta 3 7 7 80 5 40 500 -h -ngs > data/adotto_TRannotations_Regions_tandemrepeatfinder.txt
 
