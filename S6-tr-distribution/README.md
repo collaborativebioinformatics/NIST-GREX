f98154643b7b7624e42ee60d9c87ef019c3148cc
ndwarshuis <ndwar@yavin4.ch>
2022-10-11 13:42:59 -0400

    ADD results to S6

---
+++ S6-tr-distribution/README.md
@@ -0,0 +1,28 @@
+# Exploratory Analysis: understand variant size distribution in repeat regions (RRs)
+
+Goal: understand number of variants of different sizes in tandem repeats and
+homopolymers.
+
+Questions:
+* how many RRs have SVs in them?
+* how many RRs have SVs and some number of variant in the same repeat?
+* what is the average length of variant in a RR?
+* what is the variation of variant lengths in RRs?
+
+# Method
+
+Initial analysis on GIAB HG002 v4.2.1 VCF file and GIAB v3.0 TandemRepeats and
+Homopolymers stratifications bed.
+
+Pipeline:
+1. download files
+2. convert VCF to bedlike file (chrom, start, end, variant length)
+   1. remove multiallelic sites
+3. intersect strat bed with VCF bed file (left outer join)
+4. generate plots
+   1. Group variants into 4 classes based on absolute length: SNPs, INDELs
+      (1-2bp), INDELs (3-49bp), SVs
+
+# Results
+
+![image](data/chr_dist.pdf)
--- S6-tr-distribution/code/workflow/scripts/plot_chr_dist.R
+++ S6-tr-distribution/code/workflow/scripts/plot_chr_dist.R
@@ -9,36 +9,88 @@ read_bed <- function(path) {
                                      "vcf_chrom" = "c",
                                      .default = "d")) %>%
         filter(!is.na(vcf_chrom)) %>%
-        select(-vcf_chrom)
+        select(-vcf_chrom) %>%
+        mutate(chrom = as.integer(str_replace(chrom, "chr", "")),
+               start = start / 1e6,
+               end = end / 1e6) %>%
+        mutate(class = case_when(vcf_end - vcf_start == 1 & vcf_len == 0 ~ "SNPs",
+                                 vcf_len == 0 ~ "Other",
+                                 abs(vcf_len) < 3 ~ "INDELs (1-2)",
+                                 abs(vcf_len) < 50 ~ "INDELs (3-49)",
+                                 TRUE ~ "SVs") %>%
+                   factor())
 }
 
-df <- read_bed("../../results/bed/intersected.bed")
+summarize_len <- function(df) {
+    df %>%
+        group_by(chrom, start, end) %>%
+        summarize(med = median(abs(vcf_len)),
+                  mean = mean(abs(vcf_len)),
+                  sd = sd(abs(vcf_len)),
+                  max = max(abs(vcf_len)),
+                  min = min(abs(vcf_len)))
+}
 
-labeled <- df %>%
-    mutate(class = case_when(vcf_end - vcf_start == 1 & vcf_len == 0 ~ "SNPs",
-                             vcf_len == 0 ~ "Other",
-                             abs(vcf_len) < 3 ~ "INDELs (1-2)",
-                             abs(vcf_len) < 50 ~ "INDELs (3-49)",
-                             TRUE ~ "SVs") %>%
-           factor()) %>%
-    mutate(chrom = as.integer(str_replace(chrom, "chr", "")))
+group_hits <- function(df) {
+    df %>%
+        group_by(chrom, start, end, class) %>%
+        summarize(hits = n())
+}
 
-labeled %>%
-    ggplot(aes(start, fill = class)) +
-    geom_histogram() +
-    facet_wrap(~chrom, scales = "free") +
-    scale_y_log10()
+mk_chr_distr_plot <- function(df) {
+    df %>%
+        ggplot(aes(start, fill = class)) +
+        geom_histogram() +
+        facet_wrap(~chrom, scales = "free") +
+        scale_y_log10() +
+        labs(x = "pos (Mb)") +
+        theme(legend.position = "bottom")
+}
 
-grouped <- labeled %>%
-    group_by(chrom, start, end, class) %>%
-    summarize(hits = n())
+mk_chr_sum_plot <- function(stat, df) {
+    df %>%
+        ggplot(aes_string("start", stat)) +
+        geom_point(size = 0.1) +
+        facet_wrap(~chrom, scales = "free") +
+        labs(x = "pos (Mb)")
+}
 
-grouped %>%
-    ggplot(aes(hits)) +
-    geom_histogram() +
-    facet_wrap(~class, scales = "free") +
-    scale_y_log10()
+mk_chr_hist_plot <- function(df) {
+    df %>%
+        ggplot(aes(factor(class))) +
+        geom_bar() +
+        facet_wrap(~chrom, scales = "free") +
+        scale_y_log10() +
+        theme(axis.text.x = element_text(hjust = 1, angle = 90)) +
+        labs(x = "pos (Mb)")
+}
 
+mk_grouped_hist_plot <- function(df) {
+    df %>%
+        ggplot(aes(hits)) +
+        geom_histogram() +
+        facet_wrap(~class, scales = "free") +
+        scale_y_log10()
+}
 
+mk_plot <- function(df, fun, path, ...) {
+    p <- fun(df)
+    ## print(p)
+    ggsave(path, ...)
+}
 
+df <- read_bed(snakemake@input[[1]])
+sumlen <- summarize_len(df)
+grouped <- group_hits(df)
 
+sout <- snakemake@output
+
+mk_plot(df, mk_chr_distr_plot, sout$chr_dist)
+
+mk_plot(sumlen, partial(mk_chr_sum_plot, "med"), sout$med)
+mk_plot(sumlen, partial(mk_chr_sum_plot, "sd"), sout$sd)
+mk_plot(sumlen, partial(mk_chr_sum_plot, "max"), sout$max)
+
+mk_plot(df, mk_chr_hist_plot, sout$chr_hist)
+
+mk_plot(grouped, mk_grouped_hist_plot, sout$grouped)
